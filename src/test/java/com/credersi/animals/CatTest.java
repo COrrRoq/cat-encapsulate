04d9d6b62244b8028c7741a9d0429d2e41b6da4d
COrrRoq <catherine.orr@roq.co.uk>
2023-02-22 11:16:04 +0000

    Tests show more than 1 life, less than 9.

--- src/test/java/com/credersi/animals/CatTest.java
+++ src/test/java/com/credersi/animals/CatTest.java
@@ -12,4 +12,44 @@ public class CatTest {
 		assertTrue(cat.isAlive());
 		assertEquals(cat.howHungry(), 50);
 	}
+	
+	@Test
+	public void catMinusOneLife() {
+		Cat cat = new Cat();
+		assertTrue(cat.isAlive());
+		cat.runs();
+		cat.runs();
+		if(cat.howHungry() <= 100);
+		assertEquals(cat.howHungry(), 50);
+	}	
+		
+	@Test
+	public void catMinusOFourLives() {
+		Cat cat = new Cat();
+		for(int i = 0; i < 4; i++) {
+		cat.runs();
+		cat.runs();
+		}
+		assertTrue(cat.isAlive());
+	}
+	
+	@Test
+	public void catMinusNineLives() {
+		Cat cat = new Cat();
+		for(int i = 0; i < 9; i++) {
+		cat.runs();
+		cat.runs();
+		}
+		assertTrue(!cat.isAlive());
+	}
 }
+/*
+@Test
+public void testCatDead() {
+	Cat cat = new Cat();
+	assert (cat.isAlive());
+	cat.runs();
+	cat.runs();
+	if(cat.howHungry() <= 100);
+	assertTrue(cat.isAlive() == false);
+*/
